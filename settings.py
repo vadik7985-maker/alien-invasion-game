d24faaed1e508ad1ee356fa03740ad1f46dd2ce7
Vadim Matveev <vadik7985@gmail.com>
2025-12-25 12:58:26 +0300

    Создание класса Settings

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    """Класс для хранения всех настроект игры Alien Invasion."""
+
+    def __init__(self):
+        """Инициализирует настройки игры."""
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (200, 200, 200)
