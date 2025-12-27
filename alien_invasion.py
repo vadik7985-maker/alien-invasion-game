62679a38deb3720b6a26d8b6ac18a86b3e8ab651
Vadim Matveev <vadik7985@gmail.com>
2025-12-27 13:29:56 +0300

    Обработка нажатия клавиш для управления кораблем

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,6 +21,7 @@ class AlienInvasion:
         """Запуск основного цикла игры."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -28,6 +29,16 @@ class AlienInvasion:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_d:
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_a:
+                        self.ship.moving_left = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_d:
+                        self.ship.moving_right = False
+                    elif event.key == pygame.K_a:
+                        self.ship.moving_left = False
 
     def _update_screen(self):
         """Обновляет изображение на экране и отображает новый экран."""
